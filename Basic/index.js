0726790967edcba392ec5eaf142acd8020eae4df
Suphal <suphalbochkar@gmail.com>
2022-12-19 20:03:19 +0530

    Get element

--- Basic/index.js
+++ Basic/index.js
@@ -1,3 +1,7 @@
 //^ Alert
 // alert("Hello thiasdasds is JS");
 
+//^ Get element
+// document.getElementById('test').innerHTML = 5+5
+// document.getElementById('test1').innerHTML = "Iam called by js"
+
