aab87b9e172c1d2a05e97d85ca51990e6746b359
Suphal <suphalbochkar@gmail.com>
2022-12-19 20:04:07 +0530

    Add, Sub

---
+++ Calculator 1/Calci.js
@@ -0,0 +1,14 @@
+// document.getElementsByName('num1').innerHTML=11+11
+
+function addition(){
+    var a = Number(document.calculator.num1.value) ;
+    var b = Number(document.calculator.num2.value) ;
+    var sum = a + b ;
+    document.calculator.result.value=sum ;
+}
+function substract(){
+    var a = Number(document.calculator.num1.value) ;
+    var b = Number(document.calculator.num2.value) ;
+    var sub = a-b
+    document.calculator.result.value=sub
+}
