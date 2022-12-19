e60201844962085470df446b50aa5e18d309a5b5
Suphal <suphalbochkar@gmail.com>
2022-12-19 20:03:32 +0530

    Mathematical operation

--- Basic/index.js
+++ Basic/index.js
@@ -5,3 +5,17 @@
 // document.getElementById('test').innerHTML = 5+5
 // document.getElementById('test1').innerHTML = "Iam called by js"
 
+//^ Mathematical operation
+// var a=10;
+// var b=20;
+// var sum = a+b;
+// var a=10;
+// var b="asasas";
+// document.getElementById('add').innerHTML=a+b
+// document.getElementById('sub').innerHTML=a-b
+// document.getElementById('mul').innerHTML=a*b
+// document.getElementById('div').innerHTML=a/b
+// document.getElementById('pow').innerHTML=a**b
+// document.getElementById('datatype').innerHTML=typeof b
+// document.getElementById('test').innerHTML= sum;
+
