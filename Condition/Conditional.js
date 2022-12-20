1399d78d2669283b28245187b2e6bac0cc32eb44
Suphal <suphalbochkar@gmail.com>
2022-12-20 19:36:48 +0100

    Update Conditional.js

--- Condition/Conditional.js
+++ Condition/Conditional.js
@@ -17,3 +17,32 @@
     // else{
     //     document.test.value = "C Grade"
     // }
+
+//~ Loop
+    // var i= 0;
+    // while(i<=10){
+    //     document.getElementById('test').innerHTML = i;
+    //     i++;    // i= =+1 , i+=1
+    // }
+    // var i=0;
+    // var print = " ";
+    // while (i <=10){
+    //     print += "<br> the value is "+i;
+    //     i++;
+    // }
+    // document.getElementById('test').innerHTML = print ;
+
+    
+    //~ Console Log
+    // console.log(5+6);
+    // console.log("Hisdkfjd");
+
+//~ Array
+    var name = ['Suphal ','Cipher','Schools'];
+    var lpu = " ";
+    var i ;
+    for (i=0; i<name.length;i++){
+        lpu+= name[i];
+    }
+    document.getElementById('test').innerHTML=lpu;
+    document.test2.value=lpu
