a351f5b1da267b21222cdd17a8d4dabd5326a802
Suphal <suphalbochkar@gmail.com>
2022-12-20 19:36:39 +0100

    Create Conditional.js

---
+++ Condition/Conditional.js
@@ -0,0 +1,19 @@
+//~ If Else
+    // var a=24;
+    // if (age >18){
+    //     document.getElementById('test').innerHTML = "CAn vote"
+    // }
+    // else if (age<18){
+    //     document.getElementById('test').innerHTML = "CAnt vote"
+    // }
+
+    // var marks = 85;
+    // if (marks >= 85 ){
+    //     document.test.value= "A grade";
+    // }
+    // else if (marks >= 60 && marks < 80){
+    //     document.test.value = "B Grade";
+    // }
+    // else{
+    //     document.test.value = "C Grade"
+    // }
