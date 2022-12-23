e919f99acb0ef28b67a1ed7b27b0bd1507bf7bcc
Suphal <suphalbochkar@gmail.com>
2022-12-23 19:00:53 +0530

    Update sign.js

--- Signin/sign.js
+++ Signin/sign.js
@@ -2,3 +2,11 @@ var mail = document.getElementById('email');
 var key = document.getElementById('pass');
 
 var button = document.getElementById('btn');
+
+button.addEventListener("click", function(){
+    var email = mail.value;
+    var password = key.value;
+
+    // var correctEmail = "xyz.abc@gmail.com";
+    // var correctPass = "secret";
+    
