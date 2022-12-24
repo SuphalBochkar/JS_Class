444f1689467edde056c3dadd8c1a4457325b015f
Suphal <suphalbochkar@gmail.com>
2022-12-24 23:19:21 +0530

    Create js.js

---
+++ Extra/js.js
@@ -0,0 +1,20 @@
+// function dummy(a,b){
+//     var x = 5;
+//     var z = 10;
+//     return a+b;
+// }
+// var summ = x+z       
+// var x = dummy(1,2)
+// document.getElementById('test').innerHTML = x
+// console.log(dummy(1,2))
+// sub = document.querySelector('#test')
+// btn = document.querySelector('#cli')
+// btn
+
+var student = {
+    name : "Suphal",
+    subject : "JavaScript",
+    contact : 123456789,
+};
+
+document.getElementById('test').innerHTML = "This is <br>" +student.name+" and my subject is "+student.subject
